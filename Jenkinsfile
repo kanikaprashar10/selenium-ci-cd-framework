97eed42aff5a769c466206fe890382e92d66f848
kanikaprashar10 <kanikaprashar10@gmail.com>
2026-09-08 15:55:36 +0530

    Updated Code

--- Jenkinsfile
+++ Jenkinsfile
@@ -3,6 +3,7 @@ pipeline {
     agent any
 
     parameters {
+
         choice(
             name: 'BROWSER',
             choices: ['chrome', 'firefox', 'edge'],
@@ -12,10 +13,38 @@ pipeline {
 
     stages {
 
+        stage('Checkout') {
+            steps {
+                echo 'Checking out source code from Git...'
+                checkout scm
+            }
+        }
+
+        stage('Build') {
+            steps {
+                bat 'mvn clean compile'
+            }
+        }
+
         stage('Run Tests') {
             steps {
-                bat "mvn clean test -Dbrowser=${params.BROWSER}"
+                bat "mvn test -Dbrowser=${params.BROWSER}"
             }
+        }
+    }
+
+    post {
+
+        always {
+            echo 'Pipeline execution completed.'
+        }
+
+        success {
+            echo 'Tests passed successfully.'
+        }
+
+        failure {
+            echo 'Tests failed.'
         }
     }
 }
--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -1,25 +1,29 @@
 package base;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.edge.EdgeDriver;
-
+import org.openqa.selenium.edge.EdgeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
+//import org.testng.annotations.Parameters;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
     public WebDriver driver;
 
     @BeforeMethod
-    @Parameters("browser")
+   /* @Parameters("browser")
     public void setUp(String browser) {
     	
-    	//mvn clean test -Dbrowser=chrome --------on terminal
+    	//mvn test -Dbrowser=chrome --------on terminal
     	//String browser = System.getProperty("browser","chrome");
 
         if (browser.equalsIgnoreCase("chrome")) {
@@ -39,6 +43,62 @@ public class BaseClass {
         }
 
         driver.manage().window().maximize();
+        driver.get("https://www.google.com");*/
+    
+    public void setUp()
+    {
+    	// Default run chrome
+        String browser = System.getProperty("browser", "chrome");
+
+        System.out.println("Browser: " + browser);
+
+        switch (browser.toLowerCase()) {
+
+        case "chrome":
+
+            ChromeOptions chromeOptions = new ChromeOptions();
+
+            // Headless works in Jenkins and also works in Eclipse
+            chromeOptions.addArguments("--headless=new");
+            chromeOptions.addArguments("--disable-gpu");
+            chromeOptions.addArguments("--window-size=1920,1080");
+
+            driver = new ChromeDriver(chromeOptions);
+            break;
+
+        case "firefox":
+
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+            // Jenkins runs as a Windows service
+            firefoxOptions.addArguments("-headless");
+
+            driver = new FirefoxDriver(firefoxOptions);
+            break;
+
+        case "edge":
+
+            EdgeOptions edgeOptions = new EdgeOptions();
+
+            // Jenkins runs as a Windows service
+            edgeOptions.addArguments("--headless=new");
+            edgeOptions.addArguments("--disable-gpu");
+            edgeOptions.addArguments("--window-size=1920,1080");
+
+            driver = new EdgeDriver(edgeOptions);
+            break;
+
+        default:
+
+            throw new IllegalArgumentException(
+                    "Invalid browser: " + browser +
+                    ". Use chrome, firefox, or edge."
+            );
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
         driver.get("https://www.google.com");
     }
 
